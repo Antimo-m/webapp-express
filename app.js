66c6f37a0538cdf6bf0685518367d38770cd3998
Antimo-M <montellaantimo@live.it>
2026-01-19 15:19:01 +0100

    vediamo cosa ne escce, fatto il foglio .env e passate le chiavi

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 import express from "express"
+import webapp from "./web.js"
 
 const app = express ()
 const port = 3000 
 
 app.listen(port, (err) => {
     if(err) {
-        return console.err
+        return console.error("errore durante l'avvio",err)
+    }else{
+        console.log(`Benvenuto sulla porta ${port}`)
     }
 })
---
+++ web.js
@@ -0,0 +1,21 @@
+import mysql2 from "mysql2"
+
+const connection = mysql2.createConnection({
+    host : process.env.DB_HOST,
+    user :process.env.DB_USER,
+    password :process.env.DB_PASSWORD,
+    database: process.env.DB_NAME 
+})
+console.log(process.env.DB_HOST)
+
+connection.connect((err) => {
+    if (err) {
+        console.log(err)
+    }else{
+        console.log("connected to mysql")
+    }
+})
+
+
+
+export default connection
